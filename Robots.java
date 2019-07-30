4d98d9e3ae55d8cc38fb94dc46d2813f7e982ae7
acaohong <32783980+acaohong@users.noreply.github.com>
2019-07-30 16:36:58 -0700

    Create Robots.java

---
+++ Robots.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+// This class creates Robots.
+
+public class Robots {
+	private String botName;
+	private ArrayList<Task> botTasks;
+
+	public Robots (String name, ArrayList<Task> tasks) {
+		this.botName = name;
+		this.botTasks = tasks;
+	}
+
+	public String getBotName() {
+		return botName;
+	}
+
+	public void setBotName(String name) {
+		this.botName = name;
+	}
+}
